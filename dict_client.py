5fc6570bbe41c0be8b4b47eb7c728d72b6ae7828
wjj2fdd <18986709806@163.com>
2020-12-21 20:42:01 +0800

    20201221

--- dict_client.py
+++ dict_client.py
@@ -3,8 +3,30 @@ dict 客户端
 发起请求，展示结果
 """
 from socket import *
+from getpass import getpass
 
 ADDR = ("127.0.0.1", 8000)
+def do_register(s):
+    while True:
+        name = input("User:")
+        passwd = getpass()
+        passwd1 = getpass()
+        if (" " in name) or (" " in passwd):
+            print("用户名或密码不能有空格")
+            continue
+        if passwd != passwd1:
+            print("两次密码不一致")
+            continue
+        msg = "R %s %s"%(name,passwd)
+        # 发送请求
+        s.send(msg.encode())
+        # 接受反馈
+        data = s.recv(128).decode()
+        if data == "OK":
+            print("注册成功")
+        else:
+            print("注册失败")
+        return
 
 
 # 创建网络连接
--- dict_server.py
+++ dict_server.py
@@ -2,6 +2,7 @@
 dict 服务端部分
 处理请求逻辑
 """
+
 from multiprocessing import Process
 from socket import *
 import signal
@@ -13,12 +14,22 @@ HOST = "0.0.0.0"
 PORT = 8000
 ADDR = (HOST, PORT)
 
+# 处理注册
+def do_register(c,db,data):
+    tmp = data.split(" ")
+    name = tmp[1]
+    passwd = tmp[2]
 
 # 处理客户端请求
 def do_request(c, db):
+    cur = db.create_cursor() # 生成游标
     while True:
         data = c.recv(1024).decode()
-        print(data)
+        print(c.getpeername(), ":", data)
+        if data[0] == "R":
+            do_register(c,db,data)
+
+
 
 
 # 网络连接
@@ -28,14 +39,15 @@ def main():
 
     # 创建tcp套接字
     s = socket()
+    # 程序关闭端口释放
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind(ADDR)
     s.listen(5)
 
     # 处理僵尸进程(linux)
-    # signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
     # windows
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    # signal.signal(signal.SIGINT, signal.SIG_IGN)
 
     # 等待客户端链接
     print("listen the port 8000")
